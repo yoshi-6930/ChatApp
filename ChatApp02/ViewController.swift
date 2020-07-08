9729099270f427359bd9e9e47d4699040841739d
Iwamoto Yoshiki <hosomi6930@icloud.com>
2020-07-08 13:17:58 +0900

    Initial Commit

---
+++ ChatApp02/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ChatApp02
+//
+//  Created by 岩元喜輝 on 2020/07/08.
+//  Copyright © 2020 Yoshiki Iwamoto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
